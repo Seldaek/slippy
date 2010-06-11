7de8b242c6213cc034682b0d98a1dd3147b04374
Jordi Boggiano <j.boggiano@seld.be>
2010-06-11 19:50:37 +0200

    Clear alerts when changing slide and show them longer

--- src/slippy-0.9.0.js
+++ src/slippy-0.9.0.js
@@ -170,6 +170,7 @@
     };
 
     setSlide = function(num) {
+        $.clearAlerts();
         curSlide = num;
         $('.slideDisplay').text((num+1)+'/'+slides.length);
     };
@@ -207,20 +208,32 @@
 
 // Alert module
 (function($) {
-    $.extend({
-        alert: function (text) {
-            var alertWrapper = (!$('.alertWrapper').length) ? $('<div/>').addClass('alertWrapper').appendTo('body') : $('.alertWrapper');
+    var alerts = [];
 
-            $('<div/>')
-                .hide()
-                .addClass('alert')
-                .appendTo(alertWrapper)
-                .html('<p>'+text+'</p>')
-                .fadeIn(300)
-                .delay(3000)
-                .fadeOut(300, function() {
-                    $(this).remove();
-                });
-        },
-    });
+    $.alert = function (text) {
+        var alert, idx, alertWrapper = (!$('.alertWrapper').length) ? $('<div/>').addClass('alertWrapper').appendTo('body') : $('.alertWrapper');
+
+        idx = alerts.length;
+        alert = $('<div/>')
+            .hide()
+            .addClass('alert')
+            .appendTo(alertWrapper)
+            .html('<p>'+text+'</p>')
+            .fadeIn(300)
+            .delay(6000)
+            .fadeOut(300, function() {
+                $(this).remove();
+                alerts[idx] = null;
+            });
+        alerts[idx] = alert;
+    };
+
+    $.clearAlerts = function () {
+        $.each(alerts, function(idx, alert) {
+            if (alert !== null) {
+                alert.stop(true).remove();
+            }
+        });
+        alerts = [];
+    };
 })(jQuery);
